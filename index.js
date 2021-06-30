2caa97f51894ae2fb86912d58c18709dfa670b0a
Isreal6ix <isrealsix@gmail.com>
2021-06-30 07:46:31 +0700

    completed the contact section

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const container = document.querySelector('.container');
+const scrollBtn = document.querySelector('.scroll-btn');
+const html = document.querySelector('html');
 
 hamburger.addEventListener('click', () => {
-    container.classList.toggle('change');
-})
+	container.classList.toggle('change');
+});
+
+scrollBtn.addEventListener('click', () => {
+	html.style.scrollBehavior = 'smooth';
+
+	setTimeout(() => {
+		html.style.scrollBehavior = 'unset';
+	}, 1000);
+});
